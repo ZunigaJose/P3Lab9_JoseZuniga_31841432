7cf02cc541fd9a19ab160a9a60e2901f0a0d482d
Jose Zuniga <zunigajose@localhost.localdomain>
2020-03-20 19:01:28 -0600

    Avanzes en main

--- main.cpp
+++ main.cpp
@@ -1,17 +1,66 @@
 #include "Rey.hpp"
 #include "Pieza.hpp"
+#include "Dama.hpp"
+#include "Alfil.hpp"
+#include "Caballo.hpp"
+#include "Partida.hpp"
+#include "Peon.hpp"
+#include "Pieza.hpp"
+#include "Torre.hpp"
 #include <string>
 #include <iostream>
+#include <fstream>
+#include <vector>
 
 using namespace std;
 
-Pieza pieza;
+Pieza pieza, pieza2;
+Rey rey1, rey2;
+vector<Partida> partidas;
 
 int charInt(char c) {
   return c - 97;
 }
 
-bool validar(string coor) {
+int menuPieza() {
+  int op;
+  cout << "1. Dama" << endl << "2. Alfil" << endl << "3. Caballo" << endl << "4. Peon" << endl;
+  cout << "5. Torre: ";
+  cin >> op;
+  cin.ignore();
+  if (op < 1 || op > 5) {
+    cout << "Ingreso Invalido!!! Vuelva a intentar!!" << endl;
+    return menuPieza();
+  }
+  return op;
+}
+
+void stPieza() {
+  switch(menuPieza()) {
+    case 1:
+      pieza = Dama();
+      pieza2 = Dama();
+    break;
+    case 2:
+      pieza = Alfil();
+      pieza2 = Alfil();
+    break;
+    case 3:
+      pieza = Caballo();
+      pieza2 = Caballo();
+    break;
+    case 4:
+      pieza = Peon();
+      pieza2 = Peon();
+    break;
+    case 5:
+      pieza = Torre();
+      pieza2 = Torre();
+    break;
+  }
+}
+
+bool validar(string coor, bool si = false) {
     if (coor.size() != 7) {
       return false;
     }
@@ -28,9 +77,60 @@ bool validar(string coor) {
     if (act2 < 0 || act2 > 7 || nuevo2 < 0 || nuevo2 > 7) {
       return false;
     }
-    return pieza.validarMoviento(act1, act2, nuevo1, nuevo2);
+    if (si) {
+      return pieza.validarMoviento(act1, act2, nuevo1, nuevo2);
+    }
+    return true;
+}
+
+int menuP() {
+  int op;
+  cout << "0. Salir" << endl;
+  cout << "1. Juego Nuevo" << endl << "2. Recrear Partida: ";
+  cin >> op;
+  cin.ignore();
+  if (op < 0 || op > 2) {
+    return menuP();
+  }
+  return op;
+}
+
+int continuar() {
+  int op;
+  cout << "Desea continuar?" << endl << "1. Si" << endl << "0. No: ";
+  cin >> op;
+  cin.ignore();
+  if (op < 0 || op > 1) {
+    return continuar();
+  }
+  return op;
+}
+
+string ingresoPos() {
+  string coor;
+  cout << "Ingrese las coordenadas: ";
+  cin >> coor;
+  cin.ignore();
+  while (!validar(coor)) {
+    cout << "Coordenadas invalidas!!!\nIngrese de la forma [actual, nuevo]!\nVuelva a intentar: ";
+    cin >> coor;
+  }
+  return coor;
+}
+
+void jugar() {
+  stPieza();
+  do {
+    //Jugador 1
+
+  } while (continuar());
 }
 
 int main() {
+  int op, subOpP;
+  do {
+    op = menuP();
+
+  } while (op);
   return 0;
 }
